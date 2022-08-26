9785150ae5b195f442afcb2d51a84bd838ed987b
hiderbrandon <hiderbrandon@gmail.com>
2022-08-25 21:40:34 -0500

    middleware folder created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
-import { logger } from './logger.middleware';
+import { logger } from './middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
